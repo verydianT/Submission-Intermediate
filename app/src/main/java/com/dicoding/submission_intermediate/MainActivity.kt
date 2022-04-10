f1feec740803eff265711a2adc208b6e518b0956
USER <verydian.t@gmail.com>
2022-04-10 07:38:24 +0700

    progress 1

--- app/src/main/java/com/dicoding/submission_intermediate/MainActivity.kt
+++ app/src/main/java/com/dicoding/submission_intermediate/MainActivity.kt
@@ -2,10 +2,26 @@ package com.dicoding.submission_intermediate
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.dicoding.submission_intermediate.component.Button
+import com.dicoding.submission_intermediate.component.EditText
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var myButton: Button
+    private lateinit var myEditText: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        myEditText = findViewById(R.id.password)
+        myButton = findViewById(R.id.button)
+
+        setEnableButton()
+    }
+
+    private fun setEnableButton() {
+        val result = myEditText.text
+        myButton.isEnabled = result != null && result.toString().isNotEmpty()
     }
 }
